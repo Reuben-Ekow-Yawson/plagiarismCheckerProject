8f8642f51df9ac3ba98e23b572a57738147aaebb
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-20 07:03:34 +0000

    "rendering the pages here"

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { ChakraProvider, theme, } from '@chakra-ui/react';
 import Navbar from './Component/Navbar/Navbar';
+import Home from './Component/Home/Home';
+import Login from './Component/logIn/Login';
+import Dashboard from './Component/Dashboard/Dashboard';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
+      <Home />
+      <Login />
+      <Dashboard />
     </ChakraProvider>
   );
 }
