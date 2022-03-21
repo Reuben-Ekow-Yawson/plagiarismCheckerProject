a2dd505d9257d07af4079a96f5bdb9f01b339e1b
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-21 09:51:14 +0000

    "started the Routes"

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,40 +1,37 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { ChakraProvider,theme } from '@chakra-ui/react';
+import Home from './Component/Home/Home'
+import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
+import User from './Component/Dashboard/Pages/User/User/User';
+import NewUser from './Component/Dashboard/Pages/User/newUser/NewUser.jsx';
+import UserList from './Component/Dashboard/Pages/User/userList/UserList.jsx';
+import Dashboard from './Component/Dashboard/Dashboard';
+import Login from './Component/Login/Login'
+
+
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+        </Routes>
+      </Router>
+
+
+
+
+      {/* <Router>
+
+      </Router> */}
+      {/* <Home /> */}
+      {/* <UserList /> */}
+      {/* <Navbar />
+      <Login />*/}
+      {/* <Dashboard />  */}
     </ChakraProvider>
   );
 }
